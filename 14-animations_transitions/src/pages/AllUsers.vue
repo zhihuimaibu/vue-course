66fc5e3f640a7162f2b2ba661ca2a20c5f42411c
wuzhihui <wuzhihui@163.com>
2023-02-20 14:13:27 +0800

    14-animations初始化

--- 14-animations_transitions/src/pages/AllUsers.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div class="container">
-		<h2>All users</h2>
-		<router-link to="/goals">Go to course goals</router-link>
-	</div>
-</template>
--- 14-animations_transitions/src/pages/CourseGoals.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div class="container">
-		<h2>Course goals</h2>
-		<router-link to="/">Go to allusers</router-link>
-	</div>
-</template>
