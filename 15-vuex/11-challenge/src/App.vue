53c1af9216d75b81c0244dd950a8e05d262ca43f
wuzhihui <wuzhihui@163.com>
2023-02-22 12:11:33 +0800

    15-Vuex challenge完结篇

--- 15-vuex/11-challenge/src/App.vue
+++ 15-vuex/11-challenge/src/App.vue
@@ -13,84 +13,16 @@ export default {
   data() {
     return {
       isLoggedIn: false,
-      products: [
-        {
-          id: 'p1',
-          image: 'images/BookCollection.jpg',
-          title: 'Book Collection',
-          description:
-            'A collection of must-read books. All-time classics included!',
-          price: 99.99,
-        },
-        {
-          id: 'p2',
-          image: require('./images/MountainTent.jpg'),
-          title: 'Mountain Tent',
-          description: 'A tent for the ambitious outdoor tourist.',
-          price: 129.99,
-        },
-        {
-          id: 'p3',
-          image: require('./images/FoodBox.jpg'),
-          title: 'Food Box',
-          description:
-            'May be partially expired when it arrives but at least it is cheap!',
-          price: 6.99,
-        },
-        {
-          id: 'p4',
-          image: 'images/CityofDubrovnik.jpg',
-          title: 'CityofDubrovnik',
-          description: 'Game of thrones, u kno?',
-          price: 4_200_000.99,
-        },
-      ],
-      cart: { items: [], total: 0, qty: 0 },
     };
   },
   provide() {
     return {
       isLoggedIn: this.isLoggedIn,
-      products: this.products,
-      cart: this.cart,
-      addProductToCart: this.addProductToCart,
-      removeProductFromCart: this.removeProductFromCart,
       login: this.login,
       logout: this.logout,
     };
   },
   methods: {
-    addProductToCart(productData) {
-      const productInCartIndex = this.cart.items.findIndex(
-        (ci) => ci.productId === productData.id
-      );
-
-      if (productInCartIndex >= 0) {
-        this.cart.items[productInCartIndex].qty++;
-      } else {
-        const newItem = {
-          productId: productData.id,
-          title: productData.title,
-          image: productData.image,
-          price: productData.price,
-          qty: 1,
-        };
-        this.cart.items.push(newItem);
-      }
-      this.cart.qty++;
-      this.cart.total += productData.price;
-    },
-
-    removeProductFromCart(prodId) {
-      const productInCartIndex = this.cart.items.findIndex(
-        (cartItem) => cartItem.productId === prodId
-      );
-      const prodData = this.cart.items[productInCartIndex];
-      this.cart.items.splice(productInCartIndex, 1);
-      this.cart.qty -= prodData.qty;
-      this.cart.total -= prodData.price * prodData.qty;
-    },
-
     login() {
       this.isLoggedIn = true;
     },
